da657109ef7f826e40648a9c774aaeaeaebe1784
Drew MacNicholas <dmacnicholas@gmail.com>
2022-04-18 21:28:34 -0500

    Add progress on time challenge

---
+++ challenge/book.rb
@@ -0,0 +1,13 @@
+#we chose book.rb as name because this file is all about books that we like
+book_name = "Dune"
+#this is the total number of pages in the book
+pages = 212
+has_read = true
+#this is to identify if there is more than one book in the series
+has_sequel = true
+#this is the number of copies Mike owns
+copies = 0
+author = "Mike"
+
+#lotr stands for Lord of the Rings
+lotr = ["Fellowship of the Ring", "Two Towers","Return of the King"]
---
+++ challenge/index.md
@@ -0,0 +1,11 @@
+# book list
+1. Fellowship of the Ring
+2. Two Towers
+3. Return of the King
+4. The Hobbit
+5. Dune
+
+Driver - Drew
+Navigator - Mike
+We tried to be as straightforward as possible. Mike went ahead to look at what was coming next while Drew completed the current task.
+The overall process went really smooth and we were able to finish all the tasks quickly and agreed on the process and steps.
